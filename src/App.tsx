05c24f1c78c5d02511f73b784c4af63a0c1638b5
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-22 23:03:37 +0300

    feat: arrow component with props added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // import ClassComponent from "./components/ClassComponent.tsx";
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 
 function App() {
 
   return (
     <>
         <ArrowFunctionalComponent/>
+        <ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />
     </>
   )
 }
---
+++ src/components/ArrowFunctionalComponentWithProps.tsx
@@ -0,0 +1,15 @@
+/**
+ * Στα props είναι πολύ σημαντικό
+ * να δηλώνουμε τον τύπο!!!
+ * Το title θα το δηλώσουμε
+ * μέσα στο App.tsx
+ */
+type Props = {
+    title: string;
+}
+
+const ArrowFunctionalComponentWithProps = ({title}:Props) => {
+    return <h1 className={"text-center text-xl font-bold mt-12"}>{title}</h1>;
+}
+
+export default ArrowFunctionalComponentWithProps;
