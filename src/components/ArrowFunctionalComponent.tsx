3738a2ce24881f5d3594f5775a54b9c7210e7a81
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-22 22:51:05 +0300

    feat: arrow function to show simple h1 added

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,11 @@
+/**
+ * Πιο διαδεδομένη σύγχρονη
+ * μέθοδος component με
+ * arrow functions
+ */
+const ArrowFunctionalComponent = () => {
+    const title: string = "Is an Arrow Functional Component"
+    return <h1 className={"text-center mt-12 text-xl"}>{title}</h1>
+}
+
+export default ArrowFunctionalComponent
