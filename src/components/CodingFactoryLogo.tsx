d30654d162f3ccc2f93004dfc866706e8609c08d
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-23 22:58:38 +0300

    feat: cf logo component added

---
+++ src/components/CodingFactoryLogo.tsx
@@ -0,0 +1,13 @@
+const CodingFactoryLogo = () => {
+    return (
+        <>
+            <img
+                className={"h-16 mx-2"}
+                src={'/cfLogo.png'}
+                alt="CodingFactoryLogo" />
+        </>
+    )
+}
+
+
+export default CodingFactoryLogo
