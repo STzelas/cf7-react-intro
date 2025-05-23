c51843a882be41817cf0f3b6c5e50b4d0cc54067
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-23 22:58:25 +0300

    feat: all components commented out, Layout imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import ClassComponent from "./components/ClassComponent.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import CodingFactoryLogo from "./components/CodingFactoryLogo.tsx";
+
+import Layout from "./components/Layout.tsx";
 import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 
 function App() {
 
   return (
     <>
-        <ClassComponent/>
+        {/*<ClassComponent/>*/}
+        {/*<FunctionalComponent/>*/}
+        {/*<ArrowFunctionalComponent/>*/}
+        {/*<ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />*/}
+        {/*<ArrowFunctionalComponentWithPropsType*/}
+        {/*    title={"Is an Arrow Functional Component with Props"}*/}
+        {/*    description={"this is a description"}/>*/}
+        {/*<CodingFactoryLogo/>*/}
+      <Layout>
+        <h1 className={"text-center text-2xl font-bold"}>This is a title</h1>
         <FunctionalComponent/>
-        <ArrowFunctionalComponent/>
-        <ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />
-        <ArrowFunctionalComponentWithPropsType
-            title={"Is an Arrow Functional Component with Props"}
-            description={"this is a description"}/>
+      </Layout>
     </>
   )
 }
