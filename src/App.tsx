9cb0ff68b38b8f23ddcaf73c151c9ec9922f8109
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-23 10:54:11 +0300

    feat: new component with title and description added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 // import ClassComponent from "./components/ClassComponent.tsx";
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 
 function App() {
 
   return (
     <>
-        <ArrowFunctionalComponent/>
-        <ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />
+        {/*<ArrowFunctionalComponent/>*/}
+        {/*<ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />*/}
+        <ArrowFunctionalComponentWithPropsType
+            title={"Is an Arrow Functional Component with Props"}
+            description={"this is a description"}/>
     </>
   )
 }
