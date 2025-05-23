76b4889c74d2e1474f8d1db41d34c618040c9e0f
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-23 10:55:13 +0300

    feat: all components now show on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-// import ClassComponent from "./components/ClassComponent.tsx";
-// import FunctionalComponent from "./components/FunctionalComponent.tsx";
-// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+import ClassComponent from "./components/ClassComponent.tsx";
+import FunctionalComponent from "./components/FunctionalComponent.tsx";
+import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 
 function App() {
 
   return (
     <>
-        {/*<ArrowFunctionalComponent/>*/}
-        {/*<ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />*/}
+        <ClassComponent/>
+        <FunctionalComponent/>
+        <ArrowFunctionalComponent/>
+        <ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />
         <ArrowFunctionalComponentWithPropsType
             title={"Is an Arrow Functional Component with Props"}
             description={"this is a description"}/>
