f9de4b0b97c00d3887c2fffb20f80ce9d119bb84
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-24 17:48:13 +0300

    feat: children components added on a div

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@
  */
 // import React from "react";
 import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
 
 interface LayoutProps {
     children: React.ReactNode
@@ -13,7 +14,10 @@ const Layout = ({children}: LayoutProps) => {
     return (
         <>
           <Header/>
-          {children}
+          <div className="container mx-auto min-h-[95vh] pt-24">
+            {children}
+          </div>
+          <Footer/>
         </>
     )
 }
