a74868ed13e9295efddf4ee19f4cdac550edb1ae
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-24 17:47:54 +0300

    feat: more css tailwind classes added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import CodingFactoryLogo from "./CodingFactoryLogo.tsx";
 const Header = () => {
   return (
     <>
-      <header className={"bg-[#782024]"}>
-        <div className="container mx-auto flex items-center justify-between">
+      <header className={"bg-[#782024] fixed w-full"}>
+        <div className="container mx-auto px-4 flex items-center justify-between">
           <CodingFactoryLogo/>
-          <a href="/">Home</a>
+          <a className={"text-white hover:underline hover:underline-offset-4"} href="/">Home</a>
         </div>
       </header>
     </>
