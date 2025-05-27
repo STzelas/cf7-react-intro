6c1f3e3e9b5a53c8df20e76f09a63dbf4781982c
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-27 21:11:52 +0300

    feat: hook added to a new counter component

---
+++ src/components/CounterWithCustomHook.tsx
@@ -0,0 +1,25 @@
+import CounterButton from "./CounterButton.tsx";
+import {useCounter} from "../hooks/useCounter.ts";
+
+const CounterWithCustomHook = () => {
+
+  // custom hook function
+  const {count, increase, decrease, reset} = useCounter()
+
+  return (
+    <>
+      <>
+        <div className="space-y-8 pt-12">
+          <h1 className="text-center text-2xl font-semibold">Count is {count}</h1>
+          <div className="text-center space-x-4">
+            <CounterButton onClick={increase} label="Increase" addClass={"bg-green-600"}/>
+            <CounterButton onClick={decrease} label={"Decrease"} disabled={count === 0} addClass={"bg-red-600"}/>
+            <CounterButton onClick={reset} label={"Reset"} disabled={count === 0} addClass="bg-cf-dark-red"/>
+          </div>
+        </div>
+      </>
+    </>
+  )
+}
+
+export default CounterWithCustomHook;
