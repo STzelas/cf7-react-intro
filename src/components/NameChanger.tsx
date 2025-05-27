9a15332a66bd3196fa669dcf0eaab67a7aa56ee8
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-27 20:13:46 +0300

    feat: text with name change and echo added

---
+++ src/components/NameChanger.tsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+
+const NameChanger = () => {
+  const [name, setName] = useState("");
+
+  /**
+   * Στο input θέλουμε να πάρουμε το value
+   * όταν γίνει Change, όταν αλλάξει δηλαδή το μέσα
+   * να αποτυπωθεί και πάνω (στο h1)
+   */
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }
+
+  return (
+    <>
+      <h1 className="text-center text-xl pt-4">Hello, {name || "Guest"}</h1>
+      <div className={"text-center mt-4"}>
+        <input
+          type={"text"}
+          value={name}
+          onChange={handleChange}
+          className={"border px-4 py-2"}
+        />
+      </div>
+
+    </>
+  )
+}
+
+export default NameChanger
