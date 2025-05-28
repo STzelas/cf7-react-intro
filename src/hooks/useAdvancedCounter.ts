32882b198be2669956ec5b79d6cb1b995952d36a
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-28 16:20:36 +0300

    feat: new advanced hook to add lastAction with time with state

---
+++ src/hooks/useAdvancedCounter.ts
@@ -0,0 +1,65 @@
+/**
+ * Τα hooks τα ονόματα τα ξεκινάμε με
+ * Use...
+ * Μόνο κώδικας ts που περιλαμβάνει
+ * λειτουργίες, όχι ui
+ */
+import {useState} from "react";
+
+type CounterState = {
+  count: number;
+  lastAction: string;
+  time: string
+}
+
+export const useAdvancedCounter = (initialValue: number = 0) => {
+
+  const [state, setState] = useState<CounterState>({
+    count: initialValue,
+    lastAction: "",
+    time: "",
+  })
+
+  const getCurrentTime = () => new Date().toLocaleTimeString()
+
+  const increase = () => {
+    setState({
+      count: state.count + 1,
+      lastAction: "Increase",
+      time: getCurrentTime()
+    });
+  }
+
+  const decrease = () => {
+    if (state.count > 0) {
+      setState({
+        count: state.count - 1,
+        lastAction: "Decrease",
+        time: getCurrentTime()
+      });
+    }
+  }
+
+  const reset = () => {
+    if (state.count > 0) {
+      setState({
+        count: initialValue,
+        lastAction: "Reset",
+        time: getCurrentTime()
+      });
+    }
+  }
+
+  // return τις μεταβλητές και functions
+  return {
+    count: state.count,
+    lastAction: state.lastAction,
+    time: state.time,
+    increase,
+    decrease,
+    reset,
+  };
+}
+
+
+
