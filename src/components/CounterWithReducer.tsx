09c89369418cf4aef826dd9a37c6f000d0164cd4
Sot. Tzelatidis <stzelati@gmail.com>
2025-05-28 17:58:49 +0300

    feat: example counter with reducer function added

---
+++ src/components/CounterWithReducer.tsx
@@ -0,0 +1,27 @@
+import CounterButton from "./CounterButton.tsx";
+// import {useCounter} from "../hooks/useCounter.ts";
+import { useCounterWithReducer} from "../hooks/useCounterWithReducer.ts";
+
+const CounterWithReducer = () => {
+
+  // custom hook function
+  const {count, lastAction, time, increase, decrease, reset} = useCounterWithReducer();
+
+  return (
+    <>
+      <>
+        <div className="space-y-8 pt-12">
+          <h1 className="text-center text-2xl font-semibold">Count is {count}</h1>
+          <div className="text-center space-x-4">
+            <CounterButton onClick={increase} label="Increase" addClass={"bg-green-600"}/>
+            <CounterButton onClick={decrease} label={"Decrease"} disabled={count === 0} addClass={"bg-red-600"}/>
+            <CounterButton onClick={reset} label={"Reset"} disabled={count === 0} addClass="bg-cf-dark-red"/>
+          </div>
+        </div>
+        <p className={"text-center pt-8"}>Last change: <strong>{lastAction || "-"}</strong> at: <strong>{time}</strong></p>
+      </>
+    </>
+  )
+}
+
+export default CounterWithReducer;
