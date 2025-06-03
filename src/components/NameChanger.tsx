ad118c390ad444cb3d92e38fbc13fcfdf9fc5701
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-03 21:09:56 +0300

    feat: more useEffect examples and comments

--- src/components/NameChanger.tsx
+++ src/components/NameChanger.tsx
@@ -3,9 +3,26 @@ import {useEffect, useState} from "react";
 const NameChanger = () => {
   const [name, setName] = useState("");
 
+  // Έχουμε ενα setup και τα depoendancies
+  // useEffect(setup, deps?);
+
+  // const setup = () => {
+  //   document.title = name ? `Hello, ${name}` : "Hello, Stranger"
+  // }
+  //
+  // // Καλύτερα να έχουμε dependancy αλλιώς μπορεί
+  // // να έχει infinite loop
+  // useEffect(setup, [name])
+
+  // Το πάνω είναι ίδιο με το απο κάτω απλά το setup το έκανε ξεχωριστά
   useEffect(() => {
     document.title = name ? `Hello, ${name}!` : "Hello, Stranger"
   }, [name])
+
+  useEffect(() => {
+    const id: number = setInterval( () => console.log("tick"), 1000)
+    return() =>  clearInterval(id)
+  }, [])
 
   /**
    * Στο input θέλουμε να πάρουμε το value
