c0f2069cb068bdaae4dbd2e5482981bf3dda230b
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-05 23:07:18 +0300

    chore: unused imports removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-// import ClassComponent from "./components/ClassComponent.tsx";
-// import FunctionalComponent from "./components/FunctionalComponent.tsx";
-// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
-// import CodingFactoryLogo from "./components/CodingFactoryLogo.tsx";
+
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage.tsx";
 import NameChangerPage from "./pages/NameChangerPage.tsx";
-import Layout from "./components/Layout.tsx";
 import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
 import UserPage from "./pages/UserPage.tsx";
 import RouterLayout from "./components/RouterLayout.tsx";
-// import Layout from "./components/Layout.tsx";
-// import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
-// import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
-// import CounterWithReducer from "./components/CounterWithReducer.tsx";
-// import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
-// import Counter from "./components/Counter.tsx";
-// import NameChanger from "./components/NameChanger.tsx";
-// import { useEffect } from "react";
-// import OnlineStatus from "./components/OnlineStatus.tsx";
-// import Counter from "./components/Counter.tsx";
-// import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
-// import CounterAdvanced from "./components/CounterAdvanced.tsx";
-// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
-// import Todo from "./components/ToDoApp/Todo.tsx";
+
 
 
 function App() {
