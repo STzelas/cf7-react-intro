7cf67219c06edd596e4a6c92c407f223bdb46d61
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-05 18:20:18 +0300

    feat: new route logic added to App and old concept commented out

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 // import CodingFactoryLogo from "./components/CodingFactoryLogo.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage.tsx";
+import NameChangerPage from "./pages/NameChangerPage.tsx";
 import Layout from "./components/Layout.tsx";
+// import Layout from "./components/Layout.tsx";
 // import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
 // import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
 // import CounterWithReducer from "./components/CounterWithReducer.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
-import NameChanger from "./components/NameChanger.tsx";
+// import NameChanger from "./components/NameChanger.tsx";
 // import { useEffect } from "react";
-import OnlineStatus from "./components/OnlineStatus.tsx";
+// import OnlineStatus from "./components/OnlineStatus.tsx";
 // import Counter from "./components/Counter.tsx";
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
 // import CounterAdvanced from "./components/CounterAdvanced.tsx";
@@ -29,27 +33,37 @@ function App() {
 
   return (
     <>
-      <Layout>
-        <OnlineStatus/>
-        {/*<Todo />*/}
-        {/*<CounterWithReducer/>*/}
-        {/*<CounterAdvancedWithCustomHook/>*/}
-        {/*<CounterWithCustomHook/>*/}
-        {/*<CounterAdvanced/>*/}
-        {/*<CounterWithMoreStates/>*/}
-        {/*<Counter/>*/}
-        <NameChanger/>
-        {/*<FunctionalComponentWithState/>*/}
-        {/*<ClassComponentWithState/>*/}
-        {/*<ClassComponent/>*/}
-        {/*<FunctionalComponent/>*/}
-        {/*<ArrowFunctionalComponent/>*/}
-        {/*<ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />*/}
-        {/*<ArrowFunctionalComponentWithPropsType*/}
-        {/*    title={"Is an Arrow Functional Component with Props"}*/}
-        {/*    description={"this is a description"}/>*/}
-        {/*<CodingFactoryLogo/>*/}
-      </Layout>
+      {/*<Layout>*/}
+      {/*  <OnlineStatus/>*/}
+      {/*  /!*<Todo />*!/*/}
+      {/*  /!*<CounterWithReducer/>*!/*/}
+      {/*  /!*<CounterAdvancedWithCustomHook/>*!/*/}
+      {/*  /!*<CounterWithCustomHook/>*!/*/}
+      {/*  /!*<CounterAdvanced/>*!/*/}
+      {/*  /!*<CounterWithMoreStates/>*!/*/}
+      {/*  /!*<Counter/>*!/*/}
+      {/*  <NameChanger/>*/}
+      {/*  /!*<FunctionalComponentWithState/>*!/*/}
+      {/*  /!*<ClassComponentWithState/>*!/*/}
+      {/*  /!*<ClassComponent/>*!/*/}
+      {/*  /!*<FunctionalComponent/>*!/*/}
+      {/*  /!*<ArrowFunctionalComponent/>*!/*/}
+      {/*  /!*<ArrowFunctionalComponentWithProps title={"Is an Arrow Functional Component with Props"} />*!/*/}
+      {/*  /!*<ArrowFunctionalComponentWithPropsType*!/*/}
+      {/*  /!*    title={"Is an Arrow Functional Component with Props"}*!/*/}
+      {/*  /!*    description={"this is a description"}/>*!/*/}
+      {/*  /!*<CodingFactoryLogo/>*!/*/}
+      {/*</Layout>*/}
+
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path={"/"} element={<HomePage/>}/>
+            <Route path="/name-changer" element={<NameChangerPage/>}/>
+          </Routes>
+        </Layout>
+
+      </BrowserRouter>
     </>
   )
 }
