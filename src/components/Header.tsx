d33029be2b3a0dc11d34c44e092c7f8d417d24ad
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-05 18:52:21 +0300

    feat: new router links instead of a tag added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import CodingFactoryLogo from "./CodingFactoryLogo.tsx";
+import {Link} from "react-router";
 
 const Header = () => {
   return (
@@ -6,7 +7,13 @@ const Header = () => {
       <header className={"bg-[#782024] fixed w-full"}>
         <div className="container mx-auto px-4 flex items-center justify-between">
           <CodingFactoryLogo/>
-          <a className={"text-white hover:underline hover:underline-offset-4"} href="/">Home</a>
+          <nav className="flex items-center justify-between gap-8">
+            {/*<a className={"text-white hover:underline hover:underline-offset-4"} href="/">Home</a>*/}
+            <Link to="/" className={"text-white hover:underline hover:underline-offset-4"} >Home</Link>
+            <Link to="/examples/name-changer" className={"text-white hover:underline hover:underline-offset-4"} >Name Changer App</Link>
+            <Link to="/examples/online-status" className={"text-white hover:underline hover:underline-offset-4"} >Online Status App</Link>
+          </nav>
+
         </div>
       </header>
     </>
