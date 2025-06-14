b1c6418d8731d7b72f432e45ee4553d927cb6462
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-14 17:31:20 +0300

    feat: multi-field form component added

--- src/components/FormComponents/MultiFieldForm.tsx
+++ src/components/FormComponents/MultiFieldForm.tsx
@@ -1,7 +1,20 @@
 const MultiFieldForm = () => {
   return (
     <>
-
+      <form action="#" className="text-center mt-10">
+        <input
+          type="text"
+          name={"name"}
+          value={""}
+          className="px-4 py-2 border border-gray-300 rounded-lg"
+        />
+        <input
+          type="text"
+          name={"name"}
+          value={""}
+          className="px-4 py-2 border border-gray-300 rounded-lg"
+        />
+      </form>
     </>
   )
 }
